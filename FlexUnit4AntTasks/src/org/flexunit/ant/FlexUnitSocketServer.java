ab5cd643ec191648d3fb73ba206170c400c5baf5
Brian LeGros <me@brianlegros.com>
2010-03-04 00:16:17 -0500

    Updated imports.

--- FlexUnit4AntTasks/src/org/flexunit/ant/FlexUnitSocketServer.java
+++ FlexUnit4AntTasks/src/org/flexunit/ant/FlexUnitSocketServer.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
