d41ccca07a0f9c72165285bb18440584f21c1a30
Brian LeGros <me@brianlegros.com>
2010-06-21 16:50:44 -0400

    Fix for bug when running with AIR 2.0 and using file descriptor from 1.5.  Now finds verison of AIR SDK via adt.jar.

--- FlexUnit4AntTasks/src/org/flexunit/ant/launcher/commands/player/AdlCommand.java
+++ FlexUnit4AntTasks/src/org/flexunit/ant/launcher/commands/player/AdlCommand.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.Java;
 import org.apache.tools.ant.types.FilterSet;
 import org.apache.tools.ant.types.FilterSetCollection;
+import org.apache.tools.ant.types.Commandline.Argument;
 import org.apache.tools.ant.types.resources.FileResource;
 import org.apache.tools.ant.types.resources.URLResource;
 import org.apache.tools.ant.util.ResourceUtils;
@@ -13,6 +15,7 @@ import org.flexunit.ant.LoggingUtil;
 
 public class AdlCommand extends DefaultPlayerCommand
 {
+   private final String ADT_JAR_PATH = "lib" + File.separatorChar + "adt.jar";
    private final String DESCRIPTOR_TEMPLATE = "flexUnitDescriptor.template";
    private final String DESCRIPTOR_FILE = "flexUnitDescriptor.xml";
    
@@ -22,7 +25,7 @@ public class AdlCommand extends DefaultPlayerCommand
       super.setSwf(swf);
       
       getCommandLine().setExecutable(generateExecutable());
-      getCommandLine().addArguments(new String[]{swf.getParentFile().getAbsolutePath() + "/" + DESCRIPTOR_FILE});      
+      getCommandLine().addArguments(new String[]{swf.getParentFile().getAbsolutePath() + File.separatorChar + DESCRIPTOR_FILE});      
    }
 
    /**
@@ -33,14 +36,15 @@ public class AdlCommand extends DefaultPlayerCommand
       try
       {
          //Template location in JAR
-         URLResource template = new URLResource(getClass().getResource("/" + DESCRIPTOR_TEMPLATE));
+         URLResource template = new URLResource(getClass().getResource(File.separatorChar + DESCRIPTOR_TEMPLATE));
          
          //Descriptor location, same location as SWF due to relative path required in descriptor
-         File descriptor = new File(swf.getParentFile().getAbsolutePath() + "/" + DESCRIPTOR_FILE);
+         File descriptor = new File(swf.getParentFile().getAbsolutePath() + File.separatorChar + DESCRIPTOR_FILE);
          
          //Create tokens to filter
          FilterSet filters = new FilterSet();
          filters.addFilter("ADL_SWF", swf.getName());
+         filters.addFilter("ADT_VERSION", Double.toString(getVersion()));
          
          //Copy descriptor template to SWF folder performing token replacement
          ResourceUtils.copyResource(
@@ -61,6 +65,34 @@ public class AdlCommand extends DefaultPlayerCommand
       {
          throw new BuildException("Could not create application descriptor");
       }
+   }
+   
+   private double getVersion()
+   {
+      String outputProperty = "AIR_VERSION";
+      
+      //Execute mxmlc to find SDK version number
+      Java task = new Java();
+      task.setFork(true);
+      task.setFailonerror(true);
+      task.setJar(new File(getProject().getProperty("FLEX_HOME") + File.separatorChar + ADT_JAR_PATH));
+      task.setProject(getProject());
+      task.setDir(getProject().getBaseDir());
+      task.setOutputproperty(outputProperty);
+      
+      Argument versionArgument = task.createArg();
+      versionArgument.setValue("-version");
+      
+      task.execute();
+      
+      //Parse version number and return as int
+      String output = getProject().getProperty(outputProperty);
+      int prefixIndex = output.indexOf("adt version \"");
+      double version = Double.parseDouble(output.substring(prefixIndex + 13, prefixIndex + 16));
+      
+      LoggingUtil.log("Found AIR version: " + version);
+      
+      return version;
    }
    
    @Override
